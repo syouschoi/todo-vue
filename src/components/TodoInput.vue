1e977ef315ae622d5a8685bf41d6ab6267cd1397
syouschoi <java.lang.integer.parseint@gmail.com>
2020-10-26 21:05:40 +0900

    Update TodoInput.vue
    Add slot for test

--- src/components/TodoInput.vue
+++ src/components/TodoInput.vue
@@ -1,15 +1,31 @@
 <template>
-  <v-row>
-    <v-col>
-      <v-text-field
-        full-width
-        v-model="txt"
-        label="할 거 입력하셈"
-        @keyup.enter="addItem"
-      ></v-text-field>
-      <v-btn small color="primary" depressed @click="addItem">추가</v-btn>
-    </v-col>
-  </v-row>
+  <div>
+    <slot name="header">
+      <v-row>
+        <v-col>
+          <v-banner elevation="1">Test Header</v-banner>
+        </v-col>
+      </v-row>
+    </slot>
+    <slot>
+      <v-row>
+        <v-col>
+          <v-banner elevation="1">Test Slot</v-banner>
+        </v-col>
+      </v-row>
+    </slot>
+    <v-row>
+      <v-col>
+        <v-text-field
+          full-width
+          v-model="txt"
+          label="할 거 입력하셈"
+          @keyup.enter="addItem"
+        ></v-text-field>
+        <v-btn small color="primary" depressed @click="addItem">추가</v-btn>
+      </v-col>
+    </v-row>
+  </div>
 </template>
 <script>
 import { mapActions } from 'vuex';
